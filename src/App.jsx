542a594e522aec168fac523634e22df5750b5c68
yogi dhote <yrdhote99@gmail.com>
2024-06-11 18:16:02 +0530

    navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-import { useState } from 'react'
-import viteLogo from '/vite.svg'
-import './App.css'
 
+import Navbar from './components/Navbar'
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-     <p>Project_RWI</p>
+    <Navbar/>
     </>
   )
 }
 
 export default App
+
