574a8e267c66d665d563136d256c8141fe8d01f6
heyyogi <girwalyogesh@gmail.com>
2024-06-14 22:41:44 +0530

    added videoCart and vedioContainer Page

--- src/components/VideoCart.jsx
+++ src/components/VideoCart.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import Avatar from 'react-avatar'
+import API_KEY from '../constant/youtube';
 
-const VideoCart = () => {
+
+const VideoCart = ({item}) => {
+  const [channelIcon, setChannelIcon] = useState("")
+
+  const channelName = async() =>{
+    try {
+      const res = await axios.get(`https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&id=${item.snippet.channelId}&key=${API_KEY}`)
+      console.log(res);
+      setChannelIcon(res.data.items[0].snippet.thumbnails.default.url);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    channelName();
+  }, []);
+
   return (
-    <div>
-      video
+    <div className='cursor-pointer'>
+      <img className='rounded-xl w-full' src={item.snippet.thumbnails.medium.url} alt='thumbnail' />
+      <div>
+        <div className='flex mt-2 mb-4'>
+          <Avatar src={channelIcon} size={36} round={true}/>
+          <div className='ml-2'>
+            <h1 className='line-clamp-2 font-bold'>{item.snippet.title}</h1>
+            <p className='text-sm text-gray-500'>{item.snippet.channelTitle}</p>
+            <p className='text-sm text-gray-500'>{item.statistics.viewCount} views</p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/VideoContainer.jsx
+++ src/components/VideoContainer.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from "axios";
+import { YOUTUBE_VIDEO_API, API_KEY } from '../constant/youtube';
+import VideoCart from './VideoCart';
+
 
 const VideoContainer = () => {
+  const [videos, setVideos] = useState([]);
+  const fetchingYoutubeVideo = async () => {
+    try {
+      const res = await axios.get(YOUTUBE_VIDEO_API);
+      console.log(res?.data?.items);
+      setVideos(res?.data?.items)
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchingYoutubeVideo();
+  }, []);
   return (
-    <div>
-      container
+    <div className='grid grid-cols-3 gap-4'>
+      {
+        videos.map((item) => {
+          return (
+            <VideoCart key={item.id} item = {item} />
+          )
+        })
+      }
+
     </div>
   )
-}
+};
 
-export default VideoContainer
+export default VideoContainer;
